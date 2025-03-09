be68bd3659b006894348738f2b76f92b8943234b
Melrose <198425854+cobraxcodes@users.noreply.github.com>
2025-03-08 23:10:29 -0800

    Answer return odd numbers sum

--- arrays.js
+++ arrays.js
@@ -24,6 +24,17 @@ console.log(removeDuplicates([1,2,1,2,3,3,1,2,4]))
 
 // Array 3:
 // Write a function sumOddNumbers(arr) that takes an array of numbers and returns the sum of all odd numbers in the array.
+const sumOddNumbers = (arr) =>{
+  let sum = 0
+  for (let i=0; i<arr.length; i++){
+    if(arr[i] % 2 !== 0){
+        sum += arr[i]
+    }
+  }
+  return sum
+}
+
+console.log(sumOddNumbers([1,2,3,4,5]))
 
 // Array 4:
 // Write a function flattenArray(arr) that takes a nested array (an array of arrays) and flattens it into a single array. 
