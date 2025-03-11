cb260659e58425e64cd91f1e366bb7524154e319
Melrose <198425854+cobraxcodes@users.noreply.github.com>
2025-03-10 20:33:15 -0700

    Reset recursion PART ONE challenges

--- recursions.js
+++ recursions.js
@@ -1,39 +1,28 @@
 // Write a recursive function sumNumbers(n) that takes a number n and
 //  returns the sum of all numbers from 1 to n.
 
-const sumNumbers = (n) =>{
-    if(n === 1){
-        return 1
-    }
-    return n + sumNumbers (n-1)
-}
+// const sumNumbers = (arr) =>{
+//     if(arr.length === 0){
+//         return 0
+//     }
+//     return sumNumbers(arr[0] + 1)
+// }
 
-console.log(sumNumbers(10))
+
+console.log(sumNumbers(2))
 
 // Reverse a String (Recursion)
 // Write a recursive function that takes a string as input and 
 // returns the string in reverse order.
-const reverseString = (str) =>{
-    if(str === ""){
-        return ""
-    }
-    return reverseString(str.slice(1)) + str[0];
-}
 
-console.log(reverseString("Hello"))
+
 
 
 // Sum of Array (Recursion)
 // Write a recursive function that calculates 
 // the sum of all numbers in an array.
-const sumOfArray = (arr) =>{
-    if(arr.length === 0){
-        return 0
-    }
-    return arr[0] + sumOfArray(arr.slice(1))
-}
 
-console.log(sumOfArray([1,2,3,4,5]))
+
 
 
 // Exponentiation (Recursion)
