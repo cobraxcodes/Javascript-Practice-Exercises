63e230ef819f78ce15881cc688f48b789adf4b45
Melrose <198425854+cobraxcodes@users.noreply.github.com>
2025-03-11 19:41:08 -0700

    Answer while loops challenges

--- phaseOne/controlFlow/whileLoops.js
+++ phaseOne/controlFlow/whileLoops.js
@@ -1,16 +1,23 @@
+// while(condition){
+//   (logic)
+// }
+
 // Write a while loop that counts down from 10 to 1 and logs each number to the console.   
 
-let i = 10;
+let i = 10
 while(i >= 1){
-  console.log(i);
+  console.log(i)
   i--
+ 
 }
 
 // Write a while loop that calculates the sum of numbers from 1 to 100 and prints the final sum.
-let num = 1
+
+let j = 0
 let sum = 0
-while(num <= 100){
-    sum+= num
-    num++ 
+while(j <= 100){
+  sum+=j
+  j++
+
 }
 console.log(sum)
