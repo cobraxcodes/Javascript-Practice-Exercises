b1e9a2ebb799894e24d0d508646ead0a7b221031
Melrose <198425854+cobraxcodes@users.noreply.github.com>
2025-03-11 17:33:11 -0700

    Add doWhileLoops questions

---
+++ phaseOne/controlFlow/doWhileLoops.js
@@ -0,0 +1,5 @@
+// Print "Hello!" at least once, even if the condition is false.
+
+// Count from 1 to 5 using a do-while loop.
+
+// Keep generating a random number between 1 and 10 until you get 7.
