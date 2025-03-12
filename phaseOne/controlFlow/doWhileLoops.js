53cc009c6fd4d6e14d95db39eb7ef1abefef55be
Melrose <198425854+cobraxcodes@users.noreply.github.com>
2025-03-11 17:58:18 -0700

    Answer count to 5 in do while

--- phaseOne/controlFlow/doWhileLoops.js
+++ phaseOne/controlFlow/doWhileLoops.js
@@ -1,5 +1,15 @@
 // Print "Hello!" at least once, even if the condition is false.
-
+do {
+    console.log("Hello");
+}
+while(false)
 // Count from 1 to 5 using a do-while loop.
+let i = 0
+do {
+    i++
+    console.log(i)
+}
+while(i < 5)
+
 
 // Keep generating a random number between 1 and 10 until you get 7.
