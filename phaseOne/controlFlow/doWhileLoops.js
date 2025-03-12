8ec556aca1830e188daf98f879e3c713ca53c978
Melrose <198425854+cobraxcodes@users.noreply.github.com>
2025-03-11 19:19:35 -0700

    Answer random num 100 do-while

--- phaseOne/controlFlow/doWhileLoops.js
+++ phaseOne/controlFlow/doWhileLoops.js
@@ -1,15 +1,37 @@
+// do {
+//     //     (logic)
+//     // }
+//     //   while(condition)
+
+
+
 // Print "Hello!" at least once, even if the condition is false.
 do {
     console.log("Hello");
 }
 while(false)
 // Count from 1 to 5 using a do-while loop.
-let i = 0
-do {
-    i++
-    console.log(i)
-}
-while(i < 5)
-
+// let i = 0
+// do{
+//     i++
+//     console.log(i)
+// }while(i < 5)
 
 // Keep generating a random number between 1 and 10 until you get 7.
+
+// let random;
+// do{
+//     random = Math.floor(Math.random() * 10) +1
+//     console.log(random)
+// } while(random !== 7)
+//     console.log("I got a 7!")
+
+    // Write a do-while loop that generates a random number between 1 and 100 
+    // and keeps generating new random numbers until you get a number greater than 90. Log the numbers to the console.
+
+    let randomNumber;
+    do {
+        randomNumber = Math.floor(Math.random()*100) +1
+        console.log(randomNumber)
+    }while(randomNumber < 90)
+        console.log("I got a number above 90!")
--- phaseOne/controlFlow/whileLoops.js
+++ phaseOne/controlFlow/whileLoops.js
@@ -1,10 +1,10 @@
 // Write a while loop that counts down from 10 to 1 and logs each number to the console.   
 
-// let i = 10;
-// while(i >= 1){
-//   console.log(i);
-//   i--
-// }
+let i = 10;
+while(i >= 1){
+  console.log(i);
+  i--
+}
 
 // Write a while loop that calculates the sum of numbers from 1 to 100 and prints the final sum.
 let num = 1
@@ -14,5 +14,3 @@ while(num <= 100){
     num++ 
 }
 console.log(sum)
-
-// Write a program where a random number (between 1 and 10) is generated, and the user has to guess it. Keep asking until they get it right. (Use prompt() in the browser or readline in Node.js).
