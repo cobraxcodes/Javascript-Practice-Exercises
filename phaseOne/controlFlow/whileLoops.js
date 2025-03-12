f527c846c436c9acb45e1d287bbc6663ee9e0ac4
Melrose <198425854+cobraxcodes@users.noreply.github.com>
2025-03-11 17:08:39 -0700

    Answer countDown whileLoop

--- phaseOne/controlFlow/whileLoops.js
+++ phaseOne/controlFlow/whileLoops.js
@@ -1,5 +1,9 @@
-// Write a while loop that counts down from 10 to 1 and logs each number to the console.    
-
+// Write a while loop that counts down from 10 to 1 and logs each number to the console.   
+let i = 10;
+while(i >= 1){
+  console.log(i);
+  i--
+}
 // Write a while loop that calculates the sum of numbers from 1 to 100 and prints the final sum.
 
 // Write a program where a random number (between 1 and 10) is generated, and the user has to guess it. Keep asking until they get it right. (Use prompt() in the browser or readline in Node.js).
