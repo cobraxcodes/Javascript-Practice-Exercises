2abe879a79ba2fd0243719a8d7b6b8c51159241d
Melrose <198425854+cobraxcodes@users.noreply.github.com>
2025-03-12 17:12:36 -0700

    Finish objects challenges

--- phaseOne/objectsAndArrays/objects.js
+++ phaseOne/objectsAndArrays/objects.js
@@ -45,22 +45,55 @@ console.log(personalInformation.address.city)
 console.log(personalInformation.address.state)
 
 // Challenge 2: Book Collection
-
 // Create an array of objects where each object represents a book with the following properties:
-
 // title: Book title
 // author: Author name
 // year: Year of publication
 // Then, loop through the array and log each book's title and author.
+const bookCollection = [
+    {
+        title: "I am who i am",
+        author: "Unknown",
+        year: 1972
+    },
+    {
+        title: "Snapple",
+        author: "Tea",
+        year: 1988
+    },
+    {
+        title: "Salem's Lot",
+        author: "Stephen King",
+        year:  1978
+    },
+]
+bookCollection.forEach(book=>{
+    console.log(`The title of this book is ${book.title}, and the author is ${book.author} and was published on ${book.year}`)
+})
+for(let i = 0; i<bookCollection.length; i++){
+    let collections = bookCollection[i]
+    console.log(`The title of this book is ${collections.title} and the person who wrote this is ${collections.author} on ${collections.year}`)
+}
 
 // Challenge 3: Car Object
-
 // Create a car object with the following properties:
-
 // make: Car make (e.g., "Toyota")
 // model: Car model (e.g., "Camry")
 // year: Year of manufacture
 // isElectric: Boolean indicating if the car is electric or not
 // Add a method carDetails() that returns a string like: "The car is a 2020 Toyota Camry and it is electric."
 
+const car = {
+    make: "Toyota",
+    model: "Camry",
+    year: 2009,
+    isElectric: false,
+    // can add a method inside an object
+    carDetails(){
+        console.log(`The car is a ${car.year} ${car.make} ${car.model} and is not electric`)
+    }
+    
+}
+
 // Then, call the carDetails() method.
+car.carDetails()
