9d4ae4d1dd959dceb2b6aec25e010dbed2679a68
Melrose <198425854+cobraxcodes@users.noreply.github.com>
2025-03-13 21:41:38 -0700

    Set up test HTML and CSS

---
+++ DOM/BasicsDOM.js
@@ -0,0 +1,6 @@
+// Write a script that changes the text content of an HTML element with a specific ID.
+
+// Write a script that changes the background color of an element when a button is clicked.
+
+// Write a script that creates a new p element with some text and adds it to the body when a button is clicked.
+
