107acf1e900e55cc088d821cd237cf77e8aa1691
Melrose <198425854+cobraxcodes@users.noreply.github.com>
2025-03-13 22:21:08 -0700

    Create body color change function

--- DOM/BasicsDOM.js
+++ DOM/BasicsDOM.js
@@ -19,6 +19,31 @@ document.getElementById("changeTextButton").addEventListener('dblclick', dbleCli
 document.getElementById("changeTextButton").addEventListener('dblclick', warning)
 
 // Write a script that changes the background color of an element when a button is clicked.
+const colorChange = () =>{
+    document.body.style.backgroundColor= "#E88EED";
+}
+const colorAgain = () =>{
+    document.getElementById("changeBodyColorBtn").textContent= "Keep Clicking!"
+}
+const colorThree = () =>{
+    document.body.style.backgroundColor= "#D6D9CE";
+}
+const colorFour = () =>{
+    document.body.style.backgroundColor= "#D6D9CE";
+}
+const colorFive = () =>{
+    document.body.style.backgroundColor= "#C4E0F9";
+}
+
+
+document.getElementById('changeBodyColorBtn').addEventListener('click', colorChange)
+document.getElementById('changeBodyColorBtn').addEventListener('click', colorAgain)
+document.getElementById('changeBodyColorBtn').addEventListener('click', colorThree)
+document.getElementById('changeBodyColorBtn').addEventListener('click', colorFour)
+document.getElementById('changeBodyColorBtn').addEventListener('click', colorFive)
+
+
+
 
 // Write a script that creates a new p element with some text and adds it to the body when a button is clicked.
 
