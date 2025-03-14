9b93df3717090db1c4a7998207adcb5eef2d58c6
Melrose <198425854+cobraxcodes@users.noreply.github.com>
2025-03-13 23:02:22 -0700

    Finish HTML challenges 1-3

--- DOM/BasicsDOM.js
+++ DOM/BasicsDOM.js
@@ -18,7 +18,7 @@ document.getElementById("changeTextButton").addEventListener('mouseover', btnHov
 document.getElementById("changeTextButton").addEventListener('dblclick', dbleClick)
 document.getElementById("changeTextButton").addEventListener('dblclick', warning)
 
-// Write a script that changes the background color of an element when a button is clicked.
+// Write a script that changes the background color of the body when a button is clicked.
 const colorChange = () =>{
     document.body.style.backgroundColor= "#E88EED";
 }
@@ -26,24 +26,31 @@ const colorAgain = () =>{
     document.getElementById("changeBodyColorBtn").textContent= "Keep Clicking!"
 }
 const colorThree = () =>{
-    document.body.style.backgroundColor= "#D6D9CE";
+    document.body.style.backgroundColor= "pink";
 }
-const colorFour = () =>{
-    document.body.style.backgroundColor= "#D6D9CE";
+// MAIN BODY COLOR CHANGER
+const mainBodyColorChanger = () =>{
+    colorChange()
+    setTimeout(colorAgain, 1000)
+    setTimeout(colorThree, 3000)
+    setTimeout(colorChange, 5000)
 }
-const colorFive = () =>{
-    document.body.style.backgroundColor= "#C4E0F9";
+
+
+document.getElementById('changeBodyColorBtn').addEventListener('click', mainBodyColorChanger)
+
+// Write a script that changes the background color of an element when a button is clicked.
+const boxColorBtn = ()=>{
+    document.getElementById('colorBox').style.backgroundColor = "white"
 }
-
-
-document.getElementById('changeBodyColorBtn').addEventListener('click', colorChange)
-document.getElementById('changeBodyColorBtn').addEventListener('click', colorAgain)
-document.getElementById('changeBodyColorBtn').addEventListener('click', colorThree)
-document.getElementById('changeBodyColorBtn').addEventListener('click', colorFour)
-document.getElementById('changeBodyColorBtn').addEventListener('click', colorFive)
-
-
+document.getElementById('changeBox').addEventListener('click', boxColorBtn)
 
 
 // Write a script that creates a new p element with some text and adds it to the body when a button is clicked.
-
+// To create dynamic element. use .createElement() and add context using .textContext() and append(attach) document.body.appendChild(createElementVariable)
+const createElement = () =>{
+    let dynamicElement = document.createElement('p')
+    dynamicElement.textContent="Hi, I was dynamically made. Thank you for making Me!"
+    document.body.appendChild(dynamicElement)
+}
+document.getElementById('createBtn').addEventListener('click', createElement)
