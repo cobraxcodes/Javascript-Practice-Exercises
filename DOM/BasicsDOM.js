bf859564441bde7d07bccd59d5c43afc12654c32
Melrose <198425854+cobraxcodes@users.noreply.github.com>
2025-03-13 21:50:53 -0700

    Move script tag in body

--- DOM/BasicsDOM.js
+++ DOM/BasicsDOM.js
@@ -1,5 +1,9 @@
 // Write a script that changes the text content of an HTML element with a specific ID.
+const testBtn = () =>{
+    document.getElementById("message").textContent="I've been changed";
+}
 
+document.getElementById("changeTextButton").addEventListener('click', testBtn)
 // Write a script that changes the background color of an element when a button is clicked.
 
 // Write a script that creates a new p element with some text and adds it to the body when a button is clicked.
