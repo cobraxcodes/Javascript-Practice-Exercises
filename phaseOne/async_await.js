e51f39a0207d65dbb63480f8a3942baf0d3ec1dc
Melrose <198425854+cobraxcodes@users.noreply.github.com>
2025-03-21 21:49:02 -0700

    Rewrite async syntax

--- phaseOne/async_await.js
+++ phaseOne/async_await.js
@@ -1,43 +1,73 @@
-    // ASYNC SYNTAX EXAMPLE
+    // // ASYNC SYNTAX EXAMPLE
 
     //CREATING PROMISE HERE 
-    const fetchData = (url) =>{
-        // creating a promise that will either return a fail or sucessful retrieval...
-        // these params are wrapped inside because it is a function that will execute console.log
-        return new Promise ((success, fail) =>{
-            console.log("Fetching data now ... ")
+    // const fetchData = (url) =>{
+    //     // creating a promise that will either return a fail or sucessful retrieval...
+    //     // these params are wrapped inside because it is a function that will execute console.log
+    //     return new Promise ((success, fail) =>{
+    //         console.log("Fetching data now ... ")
 
 
-            // setTimeout function that will execute after 2 seconds
-        setTimeout(() =>{  
-            let random = Math.random() > 0.5
+    //         // setTimeout function that will execute after 2 seconds
+    //     setTimeout(() =>{  
+    //         let random = Math.random() > 0.5
+    //         if(random){
+    //             success("Data retrieved successfully...")
+    //         }else{
+    //             fail("Data failed to retrieve")
+    //         }
+    //     }, 2000)
+
+    //     // create a getData function 
+    //     // the async() will return the promise (the data that was gathered from fetchData())
+    //     })  
+
+    // }
+
+    // // const getData = async () =>{
+    //     // stores the code results
+    // try{
+    //     // wait for the fetchData function to get some data from "facebook.com"
+    //     // and store the result in the variable result
+    //     const result = await fetchData("facebook.com")
+    //     console.log(result)
+    //     // this will catch any errors and will log the error
+    //     // basically replaces the 
+    //     // (message) =>console.log(message), (error) =>console.log(error)
+    //     // in the old callback
+    // }catch (error){
+    //     console.log(error)
+    // }
+    // }
+    // // calling the function 
+    // getData()
+
+
+    // NOW TO REWRITE IT 
+// WITHOUT PEEKING THIS TIME
+
+const fetchData = (url) =>{
+    return new Promise ((success, fail) =>{
+        console.log("Retrieving data ...");
+
+        setTimeout (() =>{
+                let random = Math.random() > 0.5
             if(random){
-                success("Data retrieved successfully...")
+                success("Data retrieved successfully")
             }else{
                 fail("Data failed to retrieve")
             }
         }, 2000)
+    })
+}
 
-        // create a getData function 
-        // the async() will return the promise (the data that was gathered from fetchData())
-        })  
+        const getData = async () => {
+            try{
+                let result = await  fetchData("instagram.com")
+                console.log(result)
+            }catch(error){
+                console.log(error)
+            }
+        }
 
-    }
-
-    const getData = async () =>{
-        // stores the code results
-    try{
-        // wait for the fetchData function to get some data from "facebook.com"
-        // and store the result in the variable result
-        const result = await fetchData("facebook.com")
-        console.log(result)
-        // this will catch any errors and will log the error
-        // basically replaces the 
-        // (message) =>console.log(message), (error) =>console.log(error)
-        // in the old callback
-    }catch (error){
-        console.log(error)
-    }
-    }
-    // calling the function 
-    getData()
+getData()
