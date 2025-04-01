7f893ab7fe329a8e88eefca7559752156c0d338b
Melrose <198425854+cobraxcodes@users.noreply.github.com>
2025-04-01 10:27:57 -0700

    Add fetch challenges

--- phaseTwo/GET.js
+++ phaseTwo/GET.js
@@ -1,5 +1,31 @@
 // Basic Syntax
+// fetch(url)
+// .then(result => result.json())
+// .then(data => console.log(data))
+// .catch(error => console.log("Failed to fetch data", error))
+
+// Practice fetch using weather data
+const apiKey = "450910114b3d429154fcf55987d32bd0"
+const city = "Las Vegas"
+const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`
 fetch(url)
-.then(result => result.json())
-.then(data => console.log(data))
-.catch(error => console.log("Failed to fetch data", error))
+.then(response => response.json())
+.then(data => console.log("Weather data:" , data))
+.catch(error => console.log("Failed to retrieve data", error))
+
+// Challenge # 1
+// Fetch GitHub User Data
+// Use the GitHub API to fetch data about your profile.
+// API URL: https://api.github.com/users/YOUR_GITHUB_USERNAME
+// Extract and log your name, bio, and avatar URL.
+
+
+// // Challenge #2 
+// Fetch Random Dog Images 🐶
+// Use https://dog.ceo/api/breeds/image/random
+// Display the dog image in an <img> tag.
+
+// // Challenge #3
+// Fetch SpaceX Launch Data 🚀
+// Use https://api.spacexdata.com/v4/launches/latest
+// Log the mission name and launch date.
