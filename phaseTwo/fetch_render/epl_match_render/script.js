8e73fb7cee35a136eb2e0fd2c8eba3a458d7360c
Melrose <198425854+cobraxcodes@users.noreply.github.com>
2025-04-12 21:28:37 -0700

    Add script to dynamically render away team score

--- phaseTwo/fetch_render/epl_match_render/script.js
+++ phaseTwo/fetch_render/epl_match_render/script.js
@@ -1,34 +1,40 @@
 // TEAM NAMES SCRIPT
-fetch("https://www.thesportsdb.com/api/v1/json/3/eventsround.php?id=4328&r=1&s=2023-2024")
-.then(res => res.json())
-.then(res =>{
-    const teams =  res.events.slice(0,5).map(team => (team.strEvent))
-    teams.forEach(name => {
-        const createName = document.createElement('p')
-        createName.textContent = name
-        createName.id = "dynamicName"
-        teamNamesDiv.appendChild(createName)
-    })
-})
-.catch(error => console.log(error))
+// fetch("https://www.thesportsdb.com/api/v1/json/3/eventsround.php?id=4328&r=1&s=2023-2024")
+// .then(res => res.json())
+// .then(res =>{
+//     const teams =  res.events.slice(0,5).map(team => (team.strEvent))
+//     teams.forEach(name => {
+//         const createName = document.createElement('p')
+//         createName.textContent = name
+//         createName.id = "dynamicName"
+//         teamNamesDiv.appendChild(createName)
+//     })
+// })
+// .catch(error => console.log(error))
 
-// HOME SCORE
-fetch("https://www.thesportsdb.com/api/v1/json/3/eventsround.php?id=4328&r=1&s=2023-2024")
-.then(res => res.json())
-.then(res => {
- const home = res.events.slice(0,5).map(home => (home.intHomeScore))
- home.forEach(scoring => {
-    const scoreDocument = document.createElement('p')
-    scoreDocument.textContent = scoring
-    scoreDocument.id = "homeScoreDocument"
-    homeScore.appendChild(scoreDocument)
- })
-})
-.catch(error => console.log(error))
+// // HOME SCORE
+// fetch("https://www.thesportsdb.com/api/v1/json/3/eventsround.php?id=4328&r=1&s=2023-2024")
+// .then(res => res.json())
+// .then(res => {
+//  const home = res.events.slice(0,5).map(home => (home.intHomeScore))
+//  home.forEach(scoring => {
+//     const scoreDocument = document.createElement('p')
+//     scoreDocument.textContent = scoring
+//     scoreDocument.id = "homeScoreDocument"
+//     homeScore.appendChild(scoreDocument)
+//  })
+// })
+// .catch(error => console.log(error))
 
 // AWAY SCORE
 fetch("https://www.thesportsdb.com/api/v1/json/3/eventsround.php?id=4328&r=1&s=2023-2024")
 .then(res => res.json())
 .then(res => {
-    console.log(res)
+    const awayScore = res.events.slice(0,5).map(away => (away.intAwayScore))
+    awayScore.forEach(awayTeam =>{
+        const awayTeamDocument = document.createElement('p')
+        awayTeamDocument.textContent = awayTeam
+        awayScoreDiv.appendChild(awayTeamDocument)
+    })
 })
+.catch(error => console.log(error))
