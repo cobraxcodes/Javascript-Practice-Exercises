92dce9c1093ab856e22dfe4e37b06154ad955b25
Melrose <198425854+cobraxcodes@users.noreply.github.com>
2025-04-12 20:57:07 -0700

    Add script for team names to dynamically show

--- phaseTwo/fetch_render/epl_match_render/script.js
+++ phaseTwo/fetch_render/epl_match_render/script.js
@@ -1,13 +1,26 @@
-fetch('https://www.thesportsdb.com/api/v1/json/3/eventsround.php?id=4328&r=1&s=2023-2024')
+// TEAM NAMES SCRIPT
+fetch("https://www.thesportsdb.com/api/v1/json/3/eventsround.php?id=4328&r=1&s=2023-2024")
 .then(res => res.json())
-.then(data => {
-//    const teams = data.events.slice(0,3).map(match =>(`Team: ${match.strEvent} ${match.intHomeScore} - ${match.intAwayScore}`))
-//    const matches = document.createElement('h3')
-//    matches.textContent =teams
-//    matches.id = 'matches'
-//    const matchParent = document.getElementById('matchInfoContainer')
-//    matchParent.appendChild(matches)
-   console.log(data)
-}
-)
+.then(res =>{
+    const teams =  res.events.slice(0,5).map(team => (team.strEvent))
+    teams.forEach(name => {
+        const createName = document.createElement('p')
+        createName.textContent = name
+        createName.id = "dynamicName"
+        teamNamesDiv.appendChild(createName)
+    })
+})
+.catch(error => console.log(error))
+
+// HOME SCORE
+fetch("https://www.thesportsdb.com/api/v1/json/3/eventsround.php?id=4328&r=1&s=2023-2024")
+.then(res => res.json())
+.then(res => {
+ const home = res.events.slice(0,5).map(home => (home.intHomeScore))
+ home.forEach(scoring => {
+    const scoreDocument = document.createElement('p')
+    scoreDocument.textContent = scoring
+    homeScore.appendChild(scoreDocument)
+ })
+})
 .catch(error => console.log(error))
