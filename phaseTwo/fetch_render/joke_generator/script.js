9ba93d65a9c8990feba5148da27c4af268a82e2b
Melrose <198425854+cobraxcodes@users.noreply.github.com>
2025-04-17 21:57:35 -0700

    Add script to joke_generator

--- phaseTwo/fetch_render/joke_generator/script.js
+++ phaseTwo/fetch_render/joke_generator/script.js
@@ -2,3 +2,27 @@
 // Goal: Show a random joke with setup + punchline.
 // Bonus: Add a button to get a new joke on click.
 
+fetch("https://official-joke-api.appspot.com/random_joke")
+.then(res => res.json())
+.then(res =>{
+
+    //JOKE SETUP
+     const createJoke = () =>{
+        let setUpJoke = res.setup
+        let jokeDocument = document.createElement('h2')
+        jokeDocument.textContent= setUpJoke
+        jokeDocument.id = 'jokeDocument'
+        jokeContainer.appendChild(jokeDocument)
+
+        // PUNCHLINE SETUP
+        let punch = res.punchline
+        const punchDocument = document.createElement('p')
+        punchDocument.textContent = punch
+        punchDocument.id= 'punchDocument'
+        jokeDocument.appendChild(punchDocument)
+
+     }
+     document.getElementById('jokeButton').addEventListener('click', createJoke)
+   
+  
+})
