6881e671f65a24ac284ccaae344ce2aaa726303b
Melrose <198425854+cobraxcodes@users.noreply.github.com>
2025-04-18 20:48:07 -0700

    Add script for symbol

--- phaseTwo/fetch_render/crypto_tracker/script.js
+++ phaseTwo/fetch_render/crypto_tracker/script.js
@@ -13,19 +13,20 @@ fetch("https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd")
 .then(res => res.json())
 .then(res => {
     res.slice(0,5).forEach(coin =>{
-        //name
+        console.log(coin)
+
+        //name & symbol
         const name = coin.name
+        const symbol = coin.symbol
         const nameDocument = document.createElement('h3')
-        nameDocument.textContent=name
+        nameDocument.textContent= `${name}  ${symbol.toUpperCase()}`
         nameDocument.id="name"
        document.body.appendChild(nameDocument)
         console.log(coin.name)
-        
-        //
+
+        //current price
         
     })
 
-    // for each data create a document ('h3') for name
-    // h4 for symbol
     // p for price and price change percentage
 })
