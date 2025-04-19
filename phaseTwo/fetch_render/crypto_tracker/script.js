332211e642fa03f1ccb9a28bf59978b41d715bd7
Melrose <198425854+cobraxcodes@users.noreply.github.com>
2025-04-18 20:40:39 -0700

    Add script for crypto name to display

--- phaseTwo/fetch_render/crypto_tracker/script.js
+++ phaseTwo/fetch_render/crypto_tracker/script.js
@@ -8,3 +8,24 @@
 // Price change percentage
 // Logo
 // Bonus: Color code green for +change, red for –change.
+
+fetch("https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd")
+.then(res => res.json())
+.then(res => {
+    res.slice(0,5).forEach(coin =>{
+        //name
+        const name = coin.name
+        const nameDocument = document.createElement('h3')
+        nameDocument.textContent=name
+        nameDocument.id="name"
+       document.body.appendChild(nameDocument)
+        console.log(coin.name)
+        
+        //
+        
+    })
+
+    // for each data create a document ('h3') for name
+    // h4 for symbol
+    // p for price and price change percentage
+})
