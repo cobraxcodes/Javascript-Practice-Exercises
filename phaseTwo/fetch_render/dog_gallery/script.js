3989734b03362eec2888fee1e3577ebc9918545f
Melrose <198425854+cobraxcodes@users.noreply.github.com>
2025-04-18 22:28:31 -0700

    Finish with light css

--- phaseTwo/fetch_render/dog_gallery/script.js
+++ phaseTwo/fetch_render/dog_gallery/script.js
@@ -2,3 +2,21 @@
 // Goal: Fetch and display 5 random dog images in a responsive image grid.
 
 // Bonus: Add a button to reload new dogs.
+
+fetch("https://dog.ceo/api/breeds/image/random/5")
+.then(res => res.json())
+.then(res => {
+    res.message.forEach(img =>{
+        const imgDoc = document.createElement('img')
+        imgDoc.src = img
+        imgDoc.id = 'dogImages'
+        dogContainer.appendChild(imgDoc)
+    })
+})
+.catch(error => console.log(error))
+
+function reload () {
+    location.reload()
+}
+
+document.getElementById('dogsBtn').addEventListener('click', reload)
