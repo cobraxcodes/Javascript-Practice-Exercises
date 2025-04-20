2d8582c6b9c3ae746280f1a63afd9bf529293b8c
Melrose <198425854+cobraxcodes@users.noreply.github.com>
2025-04-19 21:43:35 -0700

    fetch API for NASA APOD

--- phaseTwo/fetch_render/NASA_picture/script.js
+++ phaseTwo/fetch_render/NASA_picture/script.js
@@ -5,3 +5,27 @@
 // Image
 // Description
 // Date
+
+fetch("https://api.nasa.gov/planetary/apod?api_key=M8BARK7p4Dke7weFzONxDkFC7eeeDsZ9eBepcHx8")
+.then(res => res.json())
+.then(res => {
+     // IMAGE
+   const imageDoc = document.createElement('img')
+   imageDoc.src = res.url
+   apodContainer.appendChild(imageDoc)
+     // TITLE
+   const titleDoc = document.createElement('h3')
+   titleDoc.textContent = res.title
+   apodContainer.appendChild(titleDoc)
+   //   DATE
+   const dateDoc = document.createElement('p')
+   dateDoc.textContent= res.date
+   apodContainer.appendChild(dateDoc)
+
+    // DESCRIPTION
+    const des = res.explanation
+    const desDoc = document.createElement('p')
+    desDoc.textContent = des
+    apodContainer.appendChild(desDoc)
+
+})
