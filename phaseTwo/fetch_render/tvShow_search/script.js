594003e314ffbec33f6b081aac11de271e7a8c9f
Melrose <198425854+cobraxcodes@users.noreply.github.com>
2025-04-20 01:05:57 -0700

    add script for search input showing name

--- phaseTwo/fetch_render/tvShow_search/script.js
+++ phaseTwo/fetch_render/tvShow_search/script.js
@@ -5,3 +5,22 @@
 // Title
 // Image
 // Summary
+
+document.getElementById('searchBtn').addEventListener('click', () =>{
+    const input = document.getElementById('inputBox').value.toLowerCase()
+    fetch(`https://api.tvmaze.com/search/shows?q=${input}`)
+    .then(res => res.json())
+    .then(res =>{
+        const result = res.filter(movie => movie.show.name.toLowerCase().includes(input))
+        
+        //dynamically show
+        result.forEach(movie =>{
+            moviesContainer.innerHtml = ''
+            const movieDoc = document.createElement('p')
+            movieDoc.textContent = movie.show.name
+            moviesContainer.appendChild(movieDoc)
+            
+            
+        })
+
+.catch(error => console.log(error))
