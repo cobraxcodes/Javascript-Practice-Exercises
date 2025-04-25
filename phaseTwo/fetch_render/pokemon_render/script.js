c8ce6dd96de345257abce1a9399469b838bec30e
Melrose <198425854+cobraxcodes@users.noreply.github.com>
2025-04-25 12:52:52 -0700

    Add async function to fetch pokemon API

--- phaseTwo/fetch_render/pokemon_render/script.js
+++ phaseTwo/fetch_render/pokemon_render/script.js
@@ -10,3 +10,48 @@
 // ### 🌈 Bonus:
 // - Add a search bar to look up a specific Pokémon by name.
 // - Add loading text/spinner while fetching.ol
+
+
+
+// SEARCH FUNCTION
+  // getElement searchbar and create event listener with keyup
+  document.getElementById('search').addEventListener('keyup', ()=>{
+        // get input from search bar
+        const input = document.getElementById('search').value.toLowerCase()
+        const container = document.getElementById('pokemonContainer')
+          // make sure to have inner html as empty
+        pokemonContainer.innerHTML = ' '
+
+        // if user doesn't enter anything, it doesnt render anything
+        if(input === ' ') return;
+
+
+          // fetch the data using async
+      const fetchData = async(URL) =>{
+        try{
+            // fetching the data waiting for the results
+            const fetching = await fetch(URL)
+
+            // if unable create a new error that logs message
+            if(!fetching.ok) throw new Error("Unable to fetch data")
+
+                // if able render response into json
+              const res = await res.json()
+
+               // loop through (if there's any) and create a div for each and display anything required
+            const pokemonDiv = document.createElement('div')
+            container.appendChild(pokemonDiv)
+
+              
+        }catch(Error){
+            console.error('Error')
+        }
+      }
+      fetchData(`https://pokeapi.co/api/v2/pokemon/${input}`)
+  })
+
+
+  
+
+  
+ 
