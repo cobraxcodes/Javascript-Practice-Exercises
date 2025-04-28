95fcbcee3fa1350ac27a1d758ef721a4aa3fd785
Melrose <198425854+cobraxcodes@users.noreply.github.com>
2025-04-28 00:18:46 -0700

    Add instructions for async quotesFetch

---
+++ phaseTwo/async_await/quotesList/quotesList.js
@@ -0,0 +1,12 @@
+// Challenge 5: Fetch and Display Quotes from an API
+// API: Quotes API
+// Endpoint Example: https://quote-garden.herokuapp.com/api/v3/quotes?page=1
+
+// Requirements:
+
+// Fetch a list of quotes and display the quote text along with the author.
+// Dynamically render the quotes on the page, one per line.
+// Handle errors such as:
+// Invalid or non-existent API endpoints (e.g., 404 Not Found).
+// Empty responses (e.g., no quotes available).
+// Show an error message or a "retry" button if the quotes cannot be fetched.
