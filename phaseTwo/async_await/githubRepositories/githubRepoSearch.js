3478d7de9848eea6a707334de01ca2707c242e33
Melrose <198425854+cobraxcodes@users.noreply.github.com>
2025-05-01 12:10:26 -0700

    Add async function with error message,stack,and status in try catch

--- phaseTwo/async_await/githubRepositories/githubRepoSearch.js
+++ phaseTwo/async_await/githubRepositories/githubRepoSearch.js
@@ -19,21 +19,39 @@
 // For all challenges, make sure to follow these error-handling best practices:
 
 // Try-Catch: Use try...catch blocks to handle any errors that may arise during the fetch request.
-    // fetch.status
+    // error.status
 // Error Messages: Provide clear, user-friendly error messages that describe what went wrong.
+// Network Errors: Handle network errors gracefully (e.g., the user is offline, or the server is down).
+    // error.status in try
     //error.message in catch
     // error.stack
 // Fallback Data: If the data is not available, consider displaying fallback information, such as a message like "We couldn't fetch the data at the moment, please try again later."
 // goes in the catch block
-// Network Errors: Handle network errors gracefully (e.g., the user is offline, or the server is down).
-    // error.status in try
 // Loading States: Consider adding a loading spinner or message while the data is being fetched asynchronously.
 
 
 
-// create an input box for user to type in github username
 // create a button and add an event listener "click"
-// inside the event listener define input value and fetch url with str literal 
-// create async function
+document.getElementById("searchBtn").addEventListener("click", ()=>{
+    // inside the event listener define input value and fetch url with str literal 
+    const input = document.getElementById("search").value
+
+    // create async function
+    const fetchData = async (URL) =>{
+        try{
+            const data = await fetch (URL)
+            if(!data.ok){
+                throw new Error (`Fetching failed, status: ${error.status}`)
+            }
+            const res = await data.json()
+            console.log(res)
+        }catch(error){
+            console.error(`There has been error: ${error.message} ${error.stack}`)
+        }
+    }
+    
+})
+
+
 // create a div for each repo (name, description, and link)
 // call async function
