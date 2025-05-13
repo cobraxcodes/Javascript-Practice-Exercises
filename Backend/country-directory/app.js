805c1f39f0105296389295d64121875062fefb0d
Melrose <198425854+cobraxcodes@users.noreply.github.com>
2025-05-12 17:57:31 -0700

    Add scaffolding to country-directory

--- Backend/country-directory/app.js
+++ Backend/country-directory/app.js
@@ -1,12 +1,9 @@
-🌎 Challenge 5: Country Directory
-Description:
-Display a list of countries with their name and capital city.
+// 🌎 Challenge 5: Country Directory
+// Description:
+// Display a list of countries with their name and capital city.
 
-Structure:
-
-Model: Array of countries { id, name, capital }
-
-Controller: Get the countries and render a countries.ejs view
-
-View: Show each country’s name and capital in a table.
+// Structure:
+// Model: Array of countries { id, name, capital }
+// Controller: Get the countries and render a countries.ejs view
+// View: Show each country’s name and capital in a table.
 
