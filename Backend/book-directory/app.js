9c620f797ca6d9745954f6c86566c8cf9541179a
Melrose <198425854+cobraxcodes@users.noreply.github.com>
2025-05-12 17:51:33 -0700

    Add complete book directort with mvc structure

--- Backend/book-directory/app.js
+++ Backend/book-directory/app.js
@@ -7,3 +7,23 @@
 // Controller: Get the books and render a books.ejs view
 // View: List out the books’ titles and authors in a styled list.
 
+const express = require ('express')
+const path = require('path')
+const books = require('./controller/bookController.js')
+const app = express()
+const port = 3000
+
+// templating engineer
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'ejs')
+
+// middleware
+app.use(express.static('public')) // although, no static file for now - so this line doesn't actually do anything :)
+
+//route
+app.get('/bookList', books.booksController)
+
+//listen
+app.listen(port, () =>{
+    console.log(`Server is running at port ${port}`)
+})
--- Backend/book-directory/models/books.js
+++ Backend/book-directory/models/books.js
@@ -13,4 +13,4 @@ const books =[{
    
 ]
 
-module.exports(booksModel) = () => books;
+module.exports.booksModel = () => books;
