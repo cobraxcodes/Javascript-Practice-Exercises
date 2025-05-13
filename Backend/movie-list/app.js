47d63883a98a87aa8f9124099d846dca7a1d223c
Melrose <198425854+cobraxcodes@users.noreply.github.com>
2025-05-12 22:12:26 -0700

    Add movie model

--- Backend/movie-list/app.js
+++ Backend/movie-list/app.js
@@ -1,11 +1,8 @@
-🎬 Challenge 4: Movie List with Ratings
-Description:
-Render a list of movies with their titles, release year, and IMDB rating.
+// 🎬 Challenge 4: Movie List with Ratings
+// Description:
+// Render a list of movies with their titles, release year, and IMDB rating.
 
-Structure:
-
-Model: Array of movies { id, title, year, rating }
-
-Controller: Send the movie list to movies.ejs
-
-View: Display the movies sorted by rating (bonus: highest first).
+// Structure:
+// Model: Array of movies { id, title, year }
+// Controller: Send the movie list to movies.ejs
+// View: Display the movies sorted by rating (bonus: highest first).
---
+++ Backend/movie-list/models/moviesModel.js
@@ -0,0 +1,12 @@
+const movies = [{
+    id: 1, title: "Shrek", year: 2004
+},
+{
+    id:2, title: "Solar Opposites", year: 2022
+},
+{
+    id: 3, title: "Rick and Morty", year: 2015
+}
+]
+
+module.exports.moviesModel = () => movies
