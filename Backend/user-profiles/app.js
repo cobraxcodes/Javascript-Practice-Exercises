78b25c98e52650a773701ae740810bd04fdec417
Melrose <198425854+cobraxcodes@users.noreply.github.com>
2025-05-13 08:33:33 -0700

    Add user model

--- Backend/user-profiles/app.js
+++ Backend/user-profiles/app.js
@@ -1,14 +1,10 @@
-👤 Challenge 3: User Profiles by ID
-Description:
-Create a dynamic profile page for each user based on their id.
+// 👤 Challenge 3: User Profiles by ID
+// Description:
+// Create a dynamic profile page for each user based on their id.
 
-Structure:
-
-Model: Array of users { id, name, age, email }
-
-Controller: Fetch a single user by id (from the URL param like /users/:id)
-
-View: Display the user’s name, age, and email on a profile.ejs page.
-
-Bonus: Add a "User not found" message if an invalid id is given.
+// Structure:
+// Model: Array of users { id, name, age, email }
+// Controller: Fetch a single user by id (from the URL param like /users/:id)
+// View: Display the user’s name, age, and email on a profile.ejs page.
+// Bonus: Add a "User not found" message if an invalid id is given.
 
