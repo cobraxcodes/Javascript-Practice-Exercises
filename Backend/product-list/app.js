ed2177f7d3e0cd8682b5e657cf8a04c4002788dd
Melrose <198425854+cobraxcodes@users.noreply.github.com>
2025-05-13 08:26:16 -0700

    Create product-list express server with mvc structure

--- Backend/product-list/app.js
+++ Backend/product-list/app.js
@@ -8,3 +8,25 @@
 // View: Loop through the products and display each name and price.
 
 
+const express = require('express')
+const path = require('path')
+const products = require('./controller/productController.js')
+const app = express()
+const port = 3000
+
+// templating engine
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'ejs')
+
+
+// middleware
+app.use(express.static('public')) // No static files , adding it in for consistency
+
+
+// route
+app.get('/products', products.productController)
+
+// listen
+app.listen(port, () =>{
+    console.log(`Server is listening at port ${port}`)
+})
