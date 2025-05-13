5d1e51b50f5582ff63edc4feb467714d95f64ba3
Melrose <198425854+cobraxcodes@users.noreply.github.com>
2025-05-12 22:42:08 -0700

    Add index.ejs in views directory

--- Backend/movie-list/app.js
+++ Backend/movie-list/app.js
@@ -6,3 +6,25 @@
 // Model: Array of movies { id, title, year }
 // Controller: Send the movie list to movies.ejs
 // View: Display the movies sorted by rating (bonus: highest first).
+
+const express = require('express')
+const path = require('path')
+const movies = require('./controllers/moviesController.js')
+const app = express()
+const port = 3000
+
+// templating engine
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'ejs')
+
+//middleware
+app.use(express.static('public')) // if there's static html
+
+//route
+app.get('/movieList', movies.moviesController)
+
+
+//listen
+app.listen(port, ()=>{
+    console.log(`Server is running on port:${port}`)
+})
