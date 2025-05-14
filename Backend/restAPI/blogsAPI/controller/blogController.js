2a1c487b43587216a64ddf258b6dcd40b997832e
Melrose <198425854+cobraxcodes@users.noreply.github.com>
2025-05-14 16:41:56 -0700

    Create get blog by id logic in controller and getId route in server.js

--- Backend/restAPI/blogsAPI/controller/blogController.js
+++ Backend/restAPI/blogsAPI/controller/blogController.js
@@ -1,6 +1,14 @@
 const blog = require('../models/blogModel.js')
 
+
+// GET ALL ROUTE LOGIC
 exports.getAllBlogs = (req,res) =>{
     const allBlogs = blog.blogsModel()
     res.json(allBlogs)
+}
+
+// GET A BLOG BY ID ROUTE
+exports.getBlogById = (id) =>{
+  return foundBlog = blog.blogsModel().find(x => x.id === parseInt(id));
+  
 }
--- Backend/restAPI/blogsAPI/server.js
+++ Backend/restAPI/blogsAPI/server.js
@@ -18,7 +18,17 @@ app.use(express.json())
 // GET ALL ROUTE (does not contain request or response because there's no body needed inside request and response
 app.get('/blogs', blogs.getAllBlogs)
 
+//GET a blog by id
+app.get('/blogs/:id', (req,res) =>{
+    const foundBlog = blogs.getBlogById(req.params.id)
+    if(!foundBlog){res.status(404).send(`User Not Found`)}
+   res.status(200).json(foundBlog)
+})
+
+
+
 // port
 app.listen(port, ()=>[
     console.log(`Server is listening on port ${port}`)
 ]) 
+
