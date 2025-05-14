1f980da3d38883d6feced2db4fe4ef99bca01ea5
Melrose <198425854+cobraxcodes@users.noreply.github.com>
2025-05-14 08:29:08 -0700

    Add user model

---
+++ Backend/restAPI/test/models/usersModel.js
@@ -0,0 +1,10 @@
+const users = [{
+    id: 1, name: "Cobra", email: "Cobrax@testmail.com"
+},{
+    id:2, name: "X", email: "Xemail@testmail.com"
+},{
+    id:3, name: "test", email: "testme@testmail.com"
+}
+]
+
+module.exports.getUsers = () => users
