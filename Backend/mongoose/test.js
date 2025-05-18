0f0dcb7e4940866bc52c2a0daf31b6463d8d30ae
Melrose <198425854+cobraxcodes@users.noreply.github.com>
2025-05-17 18:49:51 -0700

    Add scaffolding with mvc structure

--- Backend/mongoose/test.js
+++ Backend/mongoose/test.js
@@ -1,29 +1,21 @@
-        // BASIC MODEL / SCHEMA SYNTAX USING MONGOMEMORY SERVER
+// MONGOOSE CONNECT BASIC SYNTAX
 
-// const { MongoMemoryServer } = require('mongodb-memory-server') // importing mongo db in-memory server
-// const mongoose = require('mongoose') // importing mongoose . required odm for node and mongoose
 
-// const start = async () => { // creating a start function to connect to  in-memory server
-//   const mongoServer = await MongoMemoryServer.create() // creating the server 
-//   const uri = mongoServer.getUri() // getting the uri for the server. uri is needed to tell mongoose which server to connect to 
+// const mongoose = require('mongoose')
+// const { MongoMemoryServer } = require('mongodb-memory-server')
 
-//   await mongoose.connect(uri) // connecting to the server using uri
-//   console.log('MongoDB Memory Server connected!')
+// let mongoServer
 
-//   const taskSchema = new mongoose.Schema({ // this is the model/schema for the database
-//     name: String,
-//     done: Boolean
-//   })
-
-//   const Task = mongoose.model('Task', taskSchema) // creating the actual model and storing it in the variable named task
-
-//   await Task.create({ // this is how to create a new task
-//     name: 'Walk the Dog',
-//     done: true
-//   })
-
-//   const tasks = await Task.find() // using .find to get all task
-//   console.log(tasks)
+// async function connect() {
+//   mongoServer = await MongoMemoryServer.create()
+//   const uri = mongoServer.getUri()
+//   await mongoose.connect(uri)
+//   console.log('Connected to in-memory MongoDB')
 // }
 
-// start()
+// async function close() {
+//   await mongoose.disconnect()
+//   await mongoServer.stop()
+// }
+
+// module.exports = { connect, close }
