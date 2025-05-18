675cbc40e98a11aa54af08757ae1744d5a5d4ab3
Melrose <198425854+cobraxcodes@users.noreply.github.com>
2025-05-17 21:19:13 -0700

    Add create book logic

--- Backend/mongoose/booksDb/controller/controller.js
+++ Backend/mongoose/booksDb/controller/controller.js
@@ -1,11 +1,27 @@
 const books = require('../model/model.js')
 
-// GET ALL
+// GET ALL logic
 exports.getAll = async (req,res, next) =>{
     try{
         const allBooks = await books.find()
         res.json(allBooks)
 
+    }catch(err){
+        next(err)
+    }
+}
+
+// CREATE book logic
+exports.create() = async(req, res, next) =>{
+    try{
+        const newBook = req.body
+        const allBooks = await books.find()
+        allBooks.push(newBook)
+        res.json({
+            status: 200,
+            message: `New Book added!`,
+            book: newBook
+        })
     }catch(err){
         next(err)
     }
