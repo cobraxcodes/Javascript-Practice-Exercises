ea9ae73b9a53b306dac291d39aef359b80df0d25
Melrose <198425854+cobraxcodes@users.noreply.github.com>
2025-05-17 20:53:37 -0700

    Add get all route to server.js

--- Backend/mongoose/booksDb/server.js
+++ Backend/mongoose/booksDb/server.js
@@ -1,14 +1,33 @@
 const express = require('express')
 const app = express()
-const controller = require('./controller/controller.js')
-const post = 3001
+const books = require('./controller/controller.js')
+const port = 3001
 const connect = require('./database/database.js')
 
 // no templating engine
 
 //middleware
 app.use(express.json())
+
+
+// connecting to the in-memory server
+connect()
+
+//routes
+app.get('/books', books.getAll)
+
+
+
+
+
+// global error handler here after routes
 app.use((err, req, res, next) =>{ // global error handler
     console.error(`Stack trace: ${err.stack}`)
     res.status(404).send(`Resource Not Found`)
 });
+
+
+// port
+app.listen(port, ()=>{
+    console.log(`Server is listening on port ${port}`)
+})
