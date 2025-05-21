337846e48baa850abf5050e1142034a01a532132
Melrose <198425854+cobraxcodes@users.noreply.github.com>
2025-05-20 17:20:58 -0700

    Create createrole function and route - postman test ok

--- Backend/mongoose/rolesDb/database/database.js
+++ Backend/mongoose/rolesDb/database/database.js
@@ -1,10 +1,10 @@
 require ('dotenv') .config()
 const mongoose = require ('mongoose')
-const mongo_URI = process.env.MONGOURI || 'mongodb+srv://cobraxcodes:<db_password>@testcluster.gylfdcd.mongodb.net/booksDb?retryWrites=true&w=majority&appName=testCluster'
+const mongoURI = process.env.MONGO_URI || 'mongodb+srv://cobraxcodes:<db_password>@testcluster.gylfdcd.mongodb.net/booksDb?retryWrites=true&w=majority&appName=testCluster'
 
 const connect = async() =>{
     try{
-        await mongoose.connect(mongo_URI)
+        await mongoose.connect(mongoURI)
         console.log(`Succesfully connected to ${mongoose.connection.name} database`)
     }catch(error){
         console.log(`Database connection failed, Error: ${error.message} \nStack trace: ${error.stack}`)
---
+++ Backend/mongoose/rolesDb/server.js
@@ -0,0 +1,36 @@
+const express = require ('express')
+const {connect} = require ('./database/database.js')
+const morgan = require ('morgan')
+const roles = require ('./controller/controller.js')
+
+const app = express()
+const port = 3006
+
+
+// no templating engine
+
+//middleware (morgan, express.json)
+app.use(express.json())
+app.use(morgan(':method :url|Status: :status|Time: :response-time ms|Date: :date[clf]'))
+
+// connect to server
+const start = async() =>{
+    try{
+    await connect()
+    app.listen(port, ()=>{
+        console.log(`Server is Listening on port ${port}`)
+    })
+    }catch(error){
+       console.log(`Unable to connect to server ${error.message} \nStack Trace: ${error.stack}`)
+    }
+}
+
+start()
+// routes
+app.post('/roles/new', roles.create) // get method for creating a new role
+
+//global error handler
+app.use((err,req,res,next) =>{
+    console.err(`Something went wrong! Error: ${err.message} \n Stack Trace: ${err.stack}`)
+    res.status(404).send("Resource Not Found")
+})
