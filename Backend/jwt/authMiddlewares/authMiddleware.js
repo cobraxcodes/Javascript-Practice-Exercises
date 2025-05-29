6359f3ef3a3c8c2e27815df0c58e9bf15a872b8f
Melrose <198425854+cobraxcodes@users.noreply.github.com>
2025-05-29 12:45:44 -0700

    Add comments for the authentication middleware

--- Backend/jwt/authMiddlewares/authMiddleware.js
+++ Backend/jwt/authMiddlewares/authMiddleware.js
@@ -1,19 +1,21 @@
-const { verifyToken } = require('./jwtUtils');
+const { verifyToken } = require('./jwtUtils'); // importing verify token function from jwtUtils
 
-const authenticate = (req, res, next) => {
-  const token = req.headers.authorization?.split(' ')[1]; // Bearer token
+const authenticate = (req, res, next) => { // defining a authenticating middleware function called authenticate
+  const token = req.headers.authorization?.split(' ')[1]; //This checks the token send from the request header.  // splits the bearer (keyword to indicate what follows is a token)
+  // from the token hence the [1] so it only grabs the token part
 
   if (!token) {
-    return res.status(401).json({ message: 'No token provided' });
+    return res.status(401).json({ message: 'No token provided' }); // send if token is not provided
   }
 
-  const decoded = verifyToken(token);
+  const decoded = verifyToken(token); // if token is valid, the verify token function is the called from jwtUtils and verfies token
 
   if (!decoded) {
     return res.status(401).json({ message: 'Invalid token' });
   }
 
-  req.user = decoded;
+  req.user = decoded; // if token is valid, this attached the decoded payload into user so that any other protected routes doesn't have to decode the token again in order to see if user has
+  // access to the route depending on their role
   next();
 };
 
--- Backend/jwt/controller/controller.js
+++ Backend/jwt/controller/controller.js
@@ -1,17 +1,17 @@
 const {generateToken} = require ('./jwtUtils')
 
 const loginUser = (req,res) =>{
-    const {username, password} = req.body
+    const {username, password} = req.body // destructuring the username and password sent by user inside the request body
 }
 
 // simulate pass
-if(username === 'admin' && password === '1234'){
-    const token = generateToken ({ username, role: 'admin'})
+if(username === 'admin' && password === '1234'){  // checks if username and password are valid
+    const token = generateToken ({ username, role: 'admin'}) // if valid it generates a token using the generate token function in utils
     res.status(200).json({
-        message: 'Login sucessful', token
+        message: 'Login sucessful', token // if token generation is successful, it sends a message and the token itself
     })
 }else{
-    res.statys(401).json({
+    res.status(401).json({  // send an error if invalid
         message: 'Invalid credentials '
     })
 }
