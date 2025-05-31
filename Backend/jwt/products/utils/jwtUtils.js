3c74653ae57b553ebbd005b1386fa4f3f5a0ee54
Melrose <198425854+cobraxcodes@users.noreply.github.com>
2025-05-31 16:51:07 -0700

    Add token expiration

--- Backend/jwt/products/utils/jwtUtils.js
+++ Backend/jwt/products/utils/jwtUtils.js
@@ -2,9 +2,18 @@ const jwt = require ('jsonwebtoken')
 require ('dotenv') .config()
 
 const secretKey = process.env.JWT_SECRET;
+const options = {
+    expiresIn: '10'
+}
+
+// //{  other factors that can be used for options personalization
+//   expiresIn: '1h',
+//   issuer: 'myAppName',
+//   subject: 'userLogin'
+// }
 
 const createToken = payload =>{
-    return jwt.sign(payload, secretKey)
+    return jwt.sign(payload, secretKey, options)
 }
 
 const verifyToken = (token) =>{
